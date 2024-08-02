3610bef0b8c7b5e6ac9e353f3352ace882ae7dcf
Al Niessner <Al.Niessner@xxx.xxx>
2024-08-02 12:58:21 -0700

    use the common archive checker

--- src/main/java/gov/nasa/pds/registry/mgr/cmd/data/SetArchiveStatusCmd.java
+++ src/main/java/gov/nasa/pds/registry/mgr/cmd/data/SetArchiveStatusCmd.java
@@ -1,8 +1,5 @@
 package gov.nasa.pds.registry.mgr.cmd.data;
 
-import java.util.Set;
-import java.util.TreeSet;
-
 import org.apache.commons.cli.CommandLine;
 import gov.nasa.pds.registry.common.ConnectionFactory;
 import gov.nasa.pds.registry.common.EstablishConnectionFactory;
@@ -10,6 +7,7 @@ import gov.nasa.pds.registry.common.ResponseException;
 import gov.nasa.pds.registry.common.RestClient;
 import gov.nasa.pds.registry.common.es.dao.ProductDao;
 import gov.nasa.pds.registry.common.es.service.ProductService;
+import gov.nasa.pds.registry.common.util.ArchiveStatus;
 import gov.nasa.pds.registry.common.util.CloseUtils;
 import gov.nasa.pds.registry.mgr.cmd.CliCommand;
 
@@ -21,17 +19,11 @@ import gov.nasa.pds.registry.mgr.cmd.CliCommand;
  * @author karpenko
  */
 public class SetArchiveStatusCmd implements CliCommand {
-  private Set<String> statusNames;
-
+  private ArchiveStatus archiveStatusUtils = new ArchiveStatus();
   /**
    * Constructor
    */
   public SetArchiveStatusCmd() {
-    statusNames = new TreeSet<>();
-    statusNames.add("staged");
-    statusNames.add("archived");
-    statusNames.add("certified");
-    statusNames.add("restricted");
   }
 
     @Override
@@ -88,14 +80,9 @@ public class SetArchiveStatusCmd implements CliCommand {
     }
 
     String status = tmp.toLowerCase();
-    if (!statusNames.contains(status)) {
-      String authorized_status = String.join(", ", this.statusNames);
-      throw new Exception("Invalid '-status' parameter value: '" + tmp + "'. Authorized values are "
-          + authorized_status + ".");
-    }
+    this.archiveStatusUtils.validateStatusName(status);
     return status;
   }
-
 
   /**
    * Print help screen
@@ -109,7 +96,7 @@ public class SetArchiveStatusCmd implements CliCommand {
     System.out.println("Required parameters:");
     System.out.println("  -status <status>   One of the following values:");
 
-    for (String name : statusNames) {
+    for (String name : this.archiveStatusUtils.statusNames) {
       System.out.println("     " + name);
     }
 
